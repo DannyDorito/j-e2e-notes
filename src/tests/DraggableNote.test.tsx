39bea14d015477b95d6beec5b67911e5c1b1755f
John Allison <john@jallison.co.uk>
2023-03-17 21:42:07 +0000

    Create DraggableNote.test.tsx

---
+++ src/tests/DraggableNote.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { NoteClass } from '../classes/NoteClass';
+import DraggableNote from '../components/DraggableNote';
+
+describe('When the page is rendered', () => {
+  it('should render <DraggableNote /> TSX component', async () => {
+    const testNote: NoteClass = new NoteClass(
+      'this is a test note',
+      uuidv4(),
+      new Date().toUTCString(),
+    );
+    render(<DraggableNote note={testNote} deleteNote={undefined} editNote={undefined} />);
+
+    expect(await screen.findByText(testNote.content)).toBeInTheDocument();
+  });
+});
