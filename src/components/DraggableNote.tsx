fd8793ad6bfc4575a5b382e5019774416e1a0879
John Allison <john@jallison.co.uk>
2023-03-18 18:17:41 +0000

    re-added css and removed unused imports

--- src/components/DraggableNote.tsx
+++ src/components/DraggableNote.tsx
@@ -1,7 +1,6 @@
 import { MouseEventHandler, useState } from 'react';
 import { NoteClass } from '../classes/NoteClass';
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
-import Typography from '@mui/material/Typography/Typography';
 import Card from '@mui/material/Card/Card';
 import Box from '@mui/material/Box/Box';
 import IconButton from '@mui/material/IconButton/IconButton';
@@ -11,7 +10,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import './css/DraggableNote.css';
 
-const DraggableNote = ({
+const DraggableNote = ( {
   note,
   deleteNote,
   editNote,
@@ -19,21 +18,24 @@ const DraggableNote = ({
   note: NoteClass;
   deleteNote: MouseEventHandler<HTMLButtonElement> | undefined;
   editNote: MouseEventHandler<HTMLButtonElement> | undefined;
-}) => {
-  const [currentX, setX] = useState<number>(note.currentX);
-  const [currentY, setY] = useState<number>(note.currentY);
+} ) =>
+{
+  const [ currentX, setX ] = useState<number>( note.currentX );
+  const [ currentY, setY ] = useState<number>( note.currentY );
 
-  const [content, setContent] = useState<string>(note.content);
+  const [ content, setContent ] = useState<string>( note.content );
 
-  const handleStop = (_event: DraggableEvent, dragElement: DraggableData) => {
-    setX(dragElement.x);
-    setY(dragElement.y);
+  const handleStop = ( _event: DraggableEvent, dragElement: DraggableData ) =>
+  {
+    setX( dragElement.x );
+    setY( dragElement.y );
     note.currentX = currentX;
     note.currentY = currentY;
   };
 
-  const updateContent = (content: string) => {
-    setContent(content);
+  const updateContent = ( content: string ) =>
+  {
+    setContent( content );
     note.content = content;
   };
 
@@ -48,7 +50,8 @@ const DraggableNote = ({
           disabled={!note.edit}
           fullWidth={true}
           placeholder={'New Note'}
-          onChange={(event) => updateContent(event.target.value)}
+          type={'text'}
+          onChange={( event ) => updateContent( event.target.value )}
           sx={{
             display: 'flex',
             justifyContent: 'space-between',
