cb358321e911fdd8d82a52066abe55d73c8640c4
John Allison <john@jallison.co.uk>
2023-04-27 21:12:01 +0100

    encrypt/decrypt methods

--- src/classes/UserClass.ts
+++ src/classes/UserClass.ts
@@ -1,6 +1,7 @@
 import { version as uuidVersion, validate as uuidValidate, NIL as NIL_UUID } from 'uuid';
 import { LabelInterface } from './LabelInterface';
 import { OptionsInterface } from './OptionsInterface';
+import { GenerateKeys } from '../helpers/SecureContent';
 
 export class UserClass {
   name: string;
@@ -8,6 +9,7 @@ export class UserClass {
   authenticated: boolean;
   labels: LabelInterface[];
   options: OptionsInterface;
+  keys: CryptoKeyPair;
 
   constructor(
     name: string,
@@ -15,12 +17,14 @@ export class UserClass {
     authenticated: boolean,
     labels: LabelInterface[],
     options: OptionsInterface,
+    keys: CryptoKeyPair,
   ) {
     this.name = name;
     this.uuid = uuid;
     this.authenticated = authenticated;
     this.labels = labels;
     this.options = options;
+    this.keys = keys;
   }
 
   isAuthenticated = () => {
@@ -32,7 +36,14 @@ export class UserClass {
   };
 }
 
-export const defaultUser = new UserClass('', NIL_UUID, false, [], {
-  showNotifications: true,
-  notificationsDuration: 5000,
-});
+export const defaultUser = new UserClass(
+  '',
+  NIL_UUID,
+  false,
+  [],
+  {
+    showNotifications: true,
+    notificationsDuration: 5000,
+  },
+  await GenerateKeys(),
+);
